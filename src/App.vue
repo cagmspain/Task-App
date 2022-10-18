a54506076696676c13c7cac0381aa2bb495ca897
Carlos Alberto González Montoya <104763487+cagmspain@users.noreply.github.com>
2022-10-18 12:06:46 +0200

    second setup

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-	<h1>MI APP TASK APP</h1>
+	<div>
+		<h1>MI APP TASK APP</h1>
+	</div>
+	<ul>
+		<li><RouterLink :to="{ name: 'home' }">HOME</RouterLink></li>
+		<li><RouterLink :to="{ name: 'login' }">LOGIN</RouterLink></li>
+	</ul>
+
+	<RouterView></RouterView>
 </template>
 <script setup>
 console.log(process.env.NODE_ENV);
---
+++ src/views/Home.vue
@@ -0,0 +1,3 @@
+<template>HOME</template>
+<script setup></script>
+<style scoped></style>
